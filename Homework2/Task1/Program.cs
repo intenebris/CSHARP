d9a9eb72bb7b62d953d28afe5526001ac73c1e4c
Viaintenebris <m.manivchuk@gmail.com>
2024-06-23 02:12:27 +0300

    Completed homework2

---
+++ Homework2/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 7 == 0 && a % 23 == 0)
+        {
+            Console.WriteLine("да");
+        }
+        else
+        {
+            Console.WriteLine("нет");
+        }
+    }
+}
---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 2: Напишите программу, 
+// которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите координаты точки X и Y через пробел: ");
+        string[] coordinates = Console.ReadLine().Split(' ');
+        int x = Convert.ToInt32(coordinates[0]);
+        int y = Convert.ToInt32(coordinates[1]);
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine("1");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("2");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("3");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine("4");
+        }
+        else
+        {
+            Console.WriteLine("Точка находится на оси координат");
+        }
+    }
+}
