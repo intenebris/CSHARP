c8d80a3c03c4647e17fd437b05515b8c9d106587
Viaintenebris <m.manivchuk@gmail.com>
2024-06-22 22:11:00 +0300

    Completed Task2 in Homework1

---
+++ Homework1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Внутри класса Answer напишите метод CompareNumbers, 
+// который на вход принимает два числа и выводит, какое число большее, 
+// а какое меньшее.
+
+System.Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    System.Console.WriteLine("первое число больше");
+}else if(firstNumber < secondNumber)
+{
+    System.Console.WriteLine("второе число больше");
+}else
+{
+    System.Console.WriteLine("числа равны");
+}
---
+++ Homework1/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Внутри класса Answer напишите метод FindMax, 
+// который принимает на вход три числа и выдаёт 
+// максимальное из этих чисел.
+
+System.Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int maxNumber = a;
+
+if(maxNumber < b)
+{
+    maxNumber = b;
+    if(maxNumber < c)
+    {
+        maxNumber = c;
+        System.Console.WriteLine("Максимальное число: " + c);
+    }else
+    {
+        System.Console.WriteLine("Максимальное число: " + b);
+    }
+}else 
+{
+    System.Console.WriteLine("Максимальное число: " + a);
+}
