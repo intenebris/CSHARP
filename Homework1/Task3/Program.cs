5a4bbe09b3006989ea21a4c80c06fd2763f5919a
Viaintenebris <m.manivchuk@gmail.com>
2024-06-22 22:20:24 +0300

    Completed Task3 in Homwork1

---
+++ Homework1/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Внутри класса Answer напишите метод CheckIfEven, 
+// который на вход принимает число number и выводит, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+System.Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int checkNumber = number % 2;
+if(checkNumber == 0)
+{
+    System.Console.WriteLine("Число " + number + " является четным");
+}else
+{
+    System.Console.WriteLine("Число " + number + " не является четным");
+}
