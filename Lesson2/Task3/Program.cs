4fd7f93b9688d7585be0d988442c96a6bd4fa910
Viaintenebris <m.manivchuk@gmail.com>
2024-06-23 00:37:58 +0300

    completed all tasks in Lesson2

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// int n = 10;
+int[] array = {2, 17, 12, -14, 9, 21, 32, 36, 8, -92};
+
+int i = 0;
+
+while (i < array.Length) {
+    if (array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i++;
+}
---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = 5;
+int[] array = { 14, 6, 27, 8, 9 };
+int max = 0;
+
+for (int i = 0; i < n; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.Write($"{max} ");
