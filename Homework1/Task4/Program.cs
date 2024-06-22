c5d0f6353e6e1f0209858942b2ca6426d96f7c75
Viaintenebris <m.manivchuk@gmail.com>
2024-06-22 22:35:28 +0300

    Comleted Task4 in Homework1

---
+++ Homework1/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), 
+// а на выходе выводит все чётные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Четные числа: ");
+for(int i = 1; i <= number; i++)
+{
+    if(i % 2 == 0)
+    {
+        System.Console.Write(i + " ");
+    }
+}
