604adddeaf0631fa88ba4de1981b7503a0ddae46
Viaintenebris <m.manivchuk@gmail.com>
2024-06-30 16:17:38 +0300

    Added Lesson3:Task1

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+double result = CalculateFormula(32, 68, 21, 44);
+System.Console.WriteLine(result);
