e5891d09d14f8e5a145d5c194ed5281f310c9f49
Viaintenebris <m.manivchuk@gmail.com>
2024-07-07 16:17:42 +0300

    Added Lesson5

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычисление факториала от натурального числа N
+
+int Fact(int n)
+{
+    if(n == 1 || n ==0)
+    {
+        System.Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    System.Console.Write(" " + n);
+    return n * Fact(n-1);
+}
+
+System.Console.WriteLine(Fact(18));
